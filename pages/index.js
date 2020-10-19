7204a9f3f3f57c1bbd53be1e64f5274551671739
Paul Terry <paul.terry84@gmail.com>
2020-10-19 20:07:15 +0100

    Use JobCard component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import JobCard from '@/components/JobCard';
 import Layout from '@/components/Layout';
 
 export default function Index() {
   return (
     <>
-      <Layout />
+      <Layout>
+        <JobCard
+          companyName="Kasisto"
+          title="Front-End Software Developer"
+          type="Full time"
+          location="Remote"
+          createdAt="Mon Oct 12 15:59:27 UTC 2020"
+          companyLogo="https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBdFdMIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--09e058440662150cf22211cb95ce476343f97978/gun.io_NoBox_black%20(6).png"
+        />
+      </Layout>
     </>
   );
 }
