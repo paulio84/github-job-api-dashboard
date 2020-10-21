1501afa411f95df880e6c8cdd82e9845fdc33291
Paul Terry <paul.terry84@gmail.com>
2020-10-21 09:39:07 +0100

    created job page

---
+++ pages/job/[id].js
@@ -0,0 +1,7 @@
+import { SiteLayout } from '@/components/Layouts';
+
+const JobPage = () => <div>JobPage</div>;
+
+JobPage.getLayout = (page) => <SiteLayout>{page}</SiteLayout>;
+
+export default JobPage;
