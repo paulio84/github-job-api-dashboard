4ccbad5b4edc6719a7388f65eaf988206eb82e5c
Paul Terry <paul.terry84@gmail.com>
2020-10-31 09:16:55 +0000

    implemented search behaviour using Context API

--- components/Layouts/HomePageLayout.jsx
+++ components/Layouts/HomePageLayout.jsx
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import JobFilters from '@/components/JobFilters';
 import SearchBox from '@/components/SearchBox';
+import JobsContext from '@/lib/context';
 
-const HomePageLayout = ({ children }) => (
-  <main>
-    <div className="rounded-lg bg-blue-primary py-10 px-4 bg-searchbox bg-center md:bg-cover">
-      <SearchBox />
-    </div>
-    <div className="grid grid-cols-home lg:grid-cols-home-lg lg:gap-x-8">
-      <JobFilters />
-      {children}
-    </div>
-  </main>
-);
+const HomePageLayout = ({ children }) => {
+  const [description, setDescription] = useState('');
+  const changeDescription = (newDescription) => {
+    setDescription(newDescription);
+  };
+
+  return (
+    <main>
+      <JobsContext.Provider value={{ description, onChangeDescription: changeDescription }}>
+        <div className="rounded-lg bg-blue-primary py-10 px-4 bg-searchbox bg-center md:bg-cover">
+          <SearchBox />
+        </div>
+        <div className="grid grid-cols-home lg:grid-cols-home-lg lg:gap-x-8">
+          <JobFilters />
+          {children}
+        </div>
+      </JobsContext.Provider>
+    </main>
+  );
+};
 
 export default HomePageLayout;
--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,15 +1,39 @@
-const SearchBox = () => (
-  <div className="p-1 rounded bg-white grid grid-cols-searchbox-button md:grid-cols-searchbox-button-lg shadow-lg max-w-3xl mx-auto">
-    <i className="material-icons flex items-center justify-center text-gray-dark">work_outline</i>
-    <input
-      className="w-full py-4 pr-6 text-xs placeholder-gray-dark text-gray-dark font-roboto"
-      type="text"
-      placeholder="Title, Companies, Expertise"
-    />
-    <button className="bg-blue-secondary text-white rounded font-roboto font-medium cursor-pointer">
-      Search
-    </button>
-  </div>
-);
+import { useRef } from 'react';
+import JobsContext from '@/lib/context';
+
+const SearchBox = () => {
+  const searchRef = useRef();
+
+  return (
+    <JobsContext.Consumer>
+      {({ description, onChangeDescription }) => {
+        const onHandleClick = (e) => {
+          onChangeDescription(searchRef.current.value);
+        };
+
+        return (
+          <div className="p-1 rounded bg-white grid grid-cols-searchbox-button md:grid-cols-searchbox-button-lg shadow-lg max-w-3xl mx-auto">
+            <i className="material-icons flex items-center justify-center text-gray-dark">
+              work_outline
+            </i>
+            <input
+              className="w-full py-4 pr-6 text-xs placeholder-gray-dark text-gray-dark font-roboto"
+              type="text"
+              placeholder="Title, Companies, Expertise"
+              defaultValue={description}
+              ref={searchRef}
+            />
+            <button
+              onClick={onHandleClick}
+              className="bg-blue-secondary text-white rounded font-roboto font-medium cursor-pointer"
+            >
+              Search
+            </button>
+          </div>
+        );
+      }}
+    </JobsContext.Consumer>
+  );
+};
 
 export default SearchBox;
