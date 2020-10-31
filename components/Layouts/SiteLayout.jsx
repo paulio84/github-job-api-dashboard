7ccd8ffe7ce53216f077c8c9af4c59c0603c97af
Paul Terry <paul.terry84@gmail.com>
2020-10-31 09:41:41 +0000

    moved the context provider up to the SiteLayout

--- components/Layouts/SiteLayout.jsx
+++ components/Layouts/SiteLayout.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import Header from '@/components/Header';
+import JobsContext from '@/lib/context';
 
-const SiteLayout = ({ children }) => (
-  <div className="container px-3 md:mx-auto">
-    <Header />
-    {children}
-  </div>
-);
+const SiteLayout = ({ children }) => {
+  const [description, setDescription] = useState('');
+  const changeDescription = (newDescription) => {
+    setDescription(newDescription);
+  };
+
+  return (
+    <JobsContext.Provider value={{ description, onChangeDescription: changeDescription }}>
+      <div className="container px-3 md:mx-auto">
+        <Header />
+        {children}
+      </div>
+    </JobsContext.Provider>
+  );
+};
 
 export default SiteLayout;
